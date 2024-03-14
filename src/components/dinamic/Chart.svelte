a78f581a7bf4cdc24ebaf6d0bbc8936a44513cd0
rp4ri <rodrigo.parisusao25201@gmail.com>
2024-03-13 20:16:38 -0400

    ⚡ cambios en definicion de variables Chart.svelte para optimizar performance

--- src/components/dinamic/Chart.svelte
+++ src/components/dinamic/Chart.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { onMount, onDestroy } from 'svelte';
+    import { onMount, onDestroy, tick } from 'svelte';
     import { writable, get } from 'svelte/store';
     import { fetchChartData } from '@utils/fetchChartData';
     import TemporalitySwitch from './TemporalitySwitch.svelte';
@@ -8,6 +8,10 @@
     const prices = writable<number[]>([]);
     const labels = writable<string[]>([]);
     let canvasElement;
+
+    const aspectRatio = window.innerWidth < 768 ? 1 : 2;
+    let gradientStroke;
+    let ctx;
 
     const isLoading = writable(true);
 
@@ -20,14 +24,18 @@
     }
 
     async function loadChartData(temporality) {
-        const { prices: newPrices, labels: newLabels } = await fetchChartData(temporality);
-        prices.set(newPrices);
-        labels.set(newLabels);
-        isLoading.set(false);
-        updateChart(newPrices, newLabels);
-
-        if (temporality === 'hourly') {
-            updateCurrentPrice(newPrices);
+        try {
+            const { prices: newPrices, labels: newLabels } = await fetchChartData(temporality);
+            prices.set(newPrices);
+            labels.set(newLabels);
+            updateChart(newPrices, newLabels);
+            if (temporality === 'hourly') {
+                updateCurrentPrice(newPrices);
+            }
+        } catch (error) {
+            console.error('Error al cargar los datos del gráfico:', error);
+        } finally {
+            isLoading.set(false);
         }
     }
 
@@ -42,91 +50,99 @@
 
     async function updateChart(newPrices, newLabels) {
         if (chartInstance) {
-            chartInstance.destroy();
-        }
+            chartInstance.data.labels = newLabels;
+            chartInstance.data.datasets[0].data = newPrices;
+            chartInstance.update();
+        } else if (ctx) {
+            const Chart = (await import('chart.js/auto')).default;
 
-        const Chart = (await import('chart.js/auto')).default;
-        const ctx = canvasElement.getContext('2d');
-        const aspectRatio = window.innerWidth < 768 ? 1 : 2;
-        const gradientStroke = ctx.createLinearGradient(0, 230, 30, 20);
-
-        gradientStroke.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
-        gradientStroke.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
-        gradientStroke.addColorStop(0, 'rgba(94, 114, 228, 0)');
-
-        chartInstance = new Chart(ctx, {
-                type: 'line',
-                data: {
-                labels: newLabels,
-                datasets: [{
-                    label: 'Precio',
-                    tension: 0.4,
-                    pointRadius: 0,
-                    data: newPrices,
-                    backgroundColor: gradientStroke,
-                    borderColor: "#5e72e4",
-                    borderWidth: 3,
-                    fill: true,
-                }]
-                },
-                options: {
-                    responsive: true,
-                    aspectRatio: aspectRatio,
-                    // maintainAspectRatio: false,
-                    interaction: {
-                        intersect: false,
-                        mode: 'index',
+            chartInstance = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                    labels: newLabels,
+                    datasets: [{
+                        label: 'Precio',
+                        tension: 0.4,
+                        pointRadius: 0,
+                        data: newPrices,
+                        backgroundColor: gradientStroke,
+                        borderColor: "#5e72e4",
+                        borderWidth: 3,
+                        fill: true,
+                    }]
                     },
-                    scales: {
-                        y: {
-                            // beginAtZero: true
-                            grid: {
-                                display: true,
-                                drawOnChartArea: true,
-                                drawTicks: false,
+                    options: {
+                        responsive: true,
+                        aspectRatio: aspectRatio,
+                        // maintainAspectRatio: false,
+                        interaction: {
+                            intersect: false,
+                            mode: 'index',
+                        },
+                        scales: {
+                            y: {
+                                // beginAtZero: true
+                                grid: {
+                                    display: true,
+                                    drawOnChartArea: true,
+                                    drawTicks: false,
+                                },
+                                ticks: {
+                                    display: true,
+                                    padding: 10,
+                                    color: '#949BA8',
+                                    font: {
+                                        size: 15,
+                                        // family: "Open Sans",
+                                        style: 'normal',
+                                        lineHeight: 2
+                                    },
+                                }
                             },
-                            ticks: {
-                                display: true,
-                                padding: 10,
-                                color: '#949BA8',
-                                font: {
+                            x: {
+                                grid: {
+                                    display: false,
+                                    drawOnChartArea: false,
+                                    drawTicks: false,
+                                },
+                                ticks: {
+                                    display: true,
+                                    color: '#949BA8',
+                                    padding: 20,
+                                    font: {
                                     size: 15,
                                     // family: "Open Sans",
                                     style: 'normal',
                                     lineHeight: 2
-                                },
-                            }
-                        },
-                        x: {
-                            grid: {
-                                display: false,
-                                drawOnChartArea: false,
-                                drawTicks: false,
+                                    },
+                                }
                             },
-                            ticks: {
-                                display: true,
-                                color: '#949BA8',
-                                padding: 20,
-                                font: {
-                                size: 15,
-                                // family: "Open Sans",
-                                style: 'normal',
-                                lineHeight: 2
-                                },
-                            }
                         },
-                    },
-                    plugins: {
-                        legend: {
-                        display: false,
+                        plugins: {
+                            legend: {
+                            display: false,
+                            },
                         },
-                    },
-                }
-            });
+                    }
+                });
+            }
         }
+    
+    onMount(async () => {
+        await tick();
 
-        onMount(() => {
-        loadChartData('hourly');
+        if (canvasElement) {
+            console.log("1")
+            ctx = canvasElement.getContext('2d');
+            console.log("2")
+            gradientStroke = ctx.createLinearGradient(0, 230, 30, 20);
+            gradientStroke.addColorStop(1, 'rgba(94, 114, 228, 0.2)');
+            gradientStroke.addColorStop(0.2, 'rgba(94, 114, 228, 0.0)');
+            gradientStroke.addColorStop(0, 'rgba(94, 114, 228, 0)');
+            await loadChartData('hourly');
+        } else {
+            console.error('El elemento canvas no está disponible.');
+        }
     });
 
     onDestroy(() => {
@@ -138,11 +154,11 @@
 
 {#if $isLoading}
     <div class="loading-indicator">🔥 espera...</div>
-{:else}
-    <div class="relative h-100" style="height: 400px; width: 100%;">
-        <canvas bind:this={canvasElement}></canvas>
-    </div>
 {/if}
+
+<div class="relative h-100" style="height: 400px; width: 100%;">
+    <canvas bind:this={canvasElement}></canvas>
+</div>
 
 <div class="flex items-center justify-center">
     <div class="font-bold [#FFF]/55 mr-4">Temporalidad:</div>
